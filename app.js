543d2877786f218f2d5ad5380cdc04d010ff9013
john becerra <jfbeccera@gmail.com>
2026-03-21 20:05:10 -0500

    feat(contacto): implementar validación de campos y feedback de envío en Vanilla JS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { initMenu } from "./modules/menu.js";
 import { initSlider } from "./modules/slider.js";
 import { initScrollReveal } from "./modules/reveal.js";
 import { initScrollTop } from "./modules/scrollTop.js";
+import { initContactForm } from "./modules/contact.js";
 
 // Esperar a que el DOM esté cargado
 document.addEventListener("DOMContentLoaded", () => {
@@ -10,4 +11,5 @@ document.addEventListener("DOMContentLoaded", () => {
   // Inicializamos el módulo de animaciones
   initScrollReveal('.reveal', 0.15);  
   initScrollTop();
+  initContactForm();
 });
